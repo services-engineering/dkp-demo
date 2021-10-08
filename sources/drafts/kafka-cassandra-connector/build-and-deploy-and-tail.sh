2c88cbc4004a633d69e0e16ff0683d2a6d3f5bc5
Eric Goode <eric.goode@molex.com>
2021-10-08 15:24:54 -0500

    Moving Cassandra connector to a new repo

--- sources/drafts/kafka-cassandra-connector/build-and-deploy-and-tail.sh
+++ sources/drafts/kafka-cassandra-connector/build-and-deploy-and-tail.sh
@@ -1,12 +1,5 @@
-kubectl delete deploy/kafka-cassandra-connector
 
-docker build . -t jlnhnng/kafka-cassandra-connector
-docker push jlnhnng/kafka-cassandra-connector
+docker build . -t servicesengineering/kafka-cassandra-connector
+docker push servicesengineering/kafka-cassandra-connector
 
-sleep 2
-
-kubectl apply -f kafka-cassandra-connector.yaml
-
-sleep 5
-kubectl logs -f -l app=kafka-cassandra-connector 
 
