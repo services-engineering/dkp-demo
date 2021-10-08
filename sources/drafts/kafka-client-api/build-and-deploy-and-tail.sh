5a6e3256be59733101c535ccb913e8f9a8c5ef1b
Eric Goode <eric.goode@molex.com>
2021-10-08 15:30:17 -0500

    Changed the final one

--- sources/drafts/kafka-client-api/build-and-deploy-and-tail.sh
+++ sources/drafts/kafka-client-api/build-and-deploy-and-tail.sh
@@ -1,11 +1,4 @@
-kubectl delete deploy/kafka-client-api
 
-#docker build . -t jlnhnng/kafka-client-api
-#docker push jlnhnng/kafka-client-api
+docker build . -t servicesengineering/kafka-client-api
+docker push servicesengineering/kafka-client-api
 
-sleep 2
-
-kubectl apply -f kafka-client-api.yaml
-
-sleep 5
-kubectl logs -f -l app=kafka-client-api 
